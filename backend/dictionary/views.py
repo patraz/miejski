1abe08b1c644522ef62a0c31b1bd453c11e56640
Patryk <patrykprause@gmail.com>
2023-05-29 20:02:40 +0200

    .

--- backend/dictionary/views.py
+++ backend/dictionary/views.py
@@ -1,8 +1,4 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
@@ -13,24 +9,6 @@ from rest_framework.generics import (
 # Create your views here.
 from .serializers import DefinitionSerializer
 from .models import Definition
-
-# @api_view(['GET'])
-# def get_definitions(request):
-#     # category_id = request.GET.get('category_id', '')
-#     definitions = Definition.objects.all()
-
-#     # if category_id:
-#     #     courses = courses.filter(categories__in=[int(category_id)])
-
-#     serializer = DefinitionSerializer(definitions, many=True)
-#     return Response(serializer.data)
-
-
-
-# class AuthorDetailView(RetrieveAPIView):
-#     permission_classes = (AllowAny, )
-#     serializer_class = AuthorSerializer
-#     queryset = Author.objects.all()
 
 
 class DefinitionListView(ListAPIView):
