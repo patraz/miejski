b18053bb71559e9f2855df864976fa7ba9217de2
Patryk <patrykprause@gmail.com>
2023-06-08 15:43:23 +0200

    created about view

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -69,9 +69,9 @@
                             aria-current="page">Losuj słowo</a>
                     </li>
                     <li>
-                        <a href="#"
+                        <RouterLink :to="'/about'"
                             class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 md:dark:hover:text-emerald-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">O
-                            nas</a>
+                            nas</RouterLink>
                     </li>
                     <li>
                         <a href="#"
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <section class="bg-gradient-to-b from-neutral-900 to-neutral-800 text-white body-font">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <img class="object-cover object-center rounded" alt="hero" src="https://i.imgur.com/8b5ft4R.png">
+      </div>
+      <div
+        class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-emerald-400">O nas
+
+        </h1>
+        <p class="mb-8 leading-relaxed">Witamy na stronie Ściek - internetowego słownika slangu ulicznego, który powstaje automatycznie za pomocą ChatGPT, zaawansowanej sztucznej inteligencji stworzonej przez OpenAI. Nasza platforma jest innowacyjnym narzędziem, które gromadzi i udostępnia wyrazy, wyrażenia i skróty używane w młodzieżowym i miejskim środowisku.
+
+Nasze algorytmy analizują i klasyfikują dane, monitorując nowe trendy i zmiany w języku ulicy, aby zapewnić najświeższe i najbardziej aktualne informacje. Dzięki temu, nasz słownik jest w stanie odzwierciedlać dynamiczną i rozwijającą się naturę slangu ulicznego.
+
+Choć proces tworzenia słownika jest zautomatyzowany, staramy się zachować wysoką jakość i dokładność naszych definicji. Korzystamy z szerokiej bazy danych oraz zaawansowanych technologii przetwarzania języka naturalnego, aby dostarczyć klarowne i precyzyjne definicje, które pomogą użytkownikom zrozumieć i porozumieć się w środowisku slangowym.
+
+Chociaż nasza platforma jest oparta na sztucznej inteligencji, nasza misja pozostaje niezmieniona - dostarczanie wiarygodnych i wartościowych informacji o slangu ulicznym. Jesteśmy dumni, że możemy przyczynić się do rozwoju tej unikalnej formy komunikacji i służyć jako narzędzie dla osób zainteresowanych zgłębianiem jej tajemnic.
+
+Dziękujemy za odwiedzenie naszej strony i zachęcamy do korzystania z naszego internetowego słownika slangu ulicznego.</p>
+      </div>
   </div>
-</template>
+</section></template>
